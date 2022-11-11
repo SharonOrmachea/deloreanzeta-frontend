0337e104c9d14d15ddc4c081c68660fc005400c7
Sharon <sharon.ormachea@gmail.com>
2022-11-10 23:51:55 -0300

    generacion de carpetas con sus componentes

--- frontend/delorean/src/app/home/home.module.ts
+++ frontend/delorean/src/app/home/home.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BannerComponent } from './components/banner/banner.component';
+import { ShortcutNewsComponent } from './components/shortcut-news/shortcut-news.component';
+import { ShortcutStoreComponent } from './components/shortcut-store/shortcut-store.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BannerComponent,
+    ShortcutNewsComponent,
+    ShortcutStoreComponent
+  ],
   imports: [
     CommonModule
   ]
