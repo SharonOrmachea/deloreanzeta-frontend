e6c3d7ecba3c8b4b9466630f247cd205d923e6a9
Maria Belen Jaime <belenjaime05@gmail.com>
2022-12-24 00:57:55 -0300

    Merge branch 'validaciones' of https://github.com/Hunteando/ecommerce-s1-g1 into validaciones

--- frontend/src/app/public/login/components/form-identify-user/form-identify.component.ts
+++ frontend/src/app/public/login/components/form-identify-user/form-identify.component.ts
@@ -1,5 +1,8 @@
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-form-identify',
@@ -7,8 +10,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./form-identify.component.sass']
 })
 export class FormIdentifyComponent implements OnInit {
+ Identify = FormGroup;
 
-  constructor(private router: Router) { }
+  identify = new FormGroup ({
+    email: new FormControl ('', [Validators.required, Validators.email ,Validators.pattern('[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}')])
+})
+
+  constructor(private router: Router) {
+
+
+}
 
   ngOnInit(): void {
   }
