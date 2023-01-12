8dcc019d9a13338caccc87c72c420929637dcdac
Axel147 <lopez.b.axel@gmail.com>
2023-01-12 02:54:04 -0300

    cambios de contraseña para mail

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -14,3 +14,7 @@ export const JWT_SECRET = process.env.JWT_SECRET || "deloreanretro";
 export const JWT_SECRET_RESET = process.env.JWT_SECRET_RESET || "resetpassword";
 export const MAIL_CONTACT = process.env.MAIL_CONTACT;
 export const MAIL_CONTACT_PASSWORD = process.env.MAIL_CONTACT_PASSWORD;
+export const MAIL_CONTACT_CLIENT_ID = process.env.MAIL_CONTACT_CLIENT_ID
+export const MAIL_CONTACT_CLIENT_SECRET = process.env.MAIL_CONTACT_CLIENT_SECRET
+export const OAUTH_REFRESH_TOKEN = process.env.OAUTH_REFRESH_TOKEN
+export const OAUTH_ACCESS_TOKEN = process.env.OAUTH_ACCESS_TOKEN
