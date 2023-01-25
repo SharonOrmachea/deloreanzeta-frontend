080a199b77d5168959254817c3a0b03f8dd295a1
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2023-01-25 15:30:33 -0300

    imagenes para producto

--- backend/src/entity/Product.ts
+++ backend/src/entity/Product.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Unique, Column, ManyToOne, OneToMany} from "typeorm"
 import { MinLength, IsNotEmpty, IsOptional } from "class-validator";
 import { Category } from "./Category";
+import { Image } from "./Image";
 
 @Entity()
 @Unique(["name"])
@@ -18,12 +19,6 @@ export class Product {
     @IsNotEmpty()
     price: string;
     
-    /*@Column()
-    @MinLength(3)
-    @IsNotEmpty()
-    image: string;
-    */
-   
     @Column()
     @MinLength(10)
     @IsNotEmpty()
@@ -34,8 +29,8 @@ export class Product {
     @IsOptional()
     information: string;
 
-    @Column("simple-array")
-    images: string[];
+    @OneToMany(() => Image, image => image.product)
+    images: Image[];
 
     @ManyToOne(() => Category, category => category.products)
     category: Category;
