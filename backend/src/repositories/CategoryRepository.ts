babc399927c5fcc55445e8cd3820283c60611002
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2023-01-25 15:11:45 -0300

    creado custom repository

---
+++ backend/src/repositories/CategoryRepository.ts
@@ -0,0 +1,28 @@
+import { EntityRepository, getCustomRepository, Repository } from 'typeorm';
+import { Category } from '../entity/Category';
+
+@EntityRepository(Category)
+export class CategoryRepository extends Repository<Category> {
+    // custom methods
+    findByName(name: string) {
+        return this.findOneBy({ name });
+    }
+    findById(id: number) {
+        return this.findOneBy({ id });
+    }
+    async findAll(): Promise<Array<Category>> {
+        const categories = await this.createQueryBuilder('category')
+            .getMany();
+        return categories;
+    }
+    async saveCategory(category: Category): Promise<Category> {
+        return await this.save(category);
+    }
+    async createCategory(category: Category): Promise<Category> {
+        return await this.create(category);
+    }
+}
+
+export const getCategoryRepository = () => {
+    return getCustomRepository(CategoryRepository);
+}
