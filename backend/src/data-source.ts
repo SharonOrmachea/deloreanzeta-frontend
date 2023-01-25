6b33907f99501540b0e532ac285441edcc612366
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2023-01-25 15:10:27 -0300

    seteada la config de typeorm como export de objeto plano

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,28 +1,20 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { Category } from "./entity/Category"
-import { Image } from "./entity/Image"
-import { Product } from "./entity/Product"
-import { User } from "./entity/User"
+import 'reflect-metadata';
+import { Category } from './entity/Category';
+import { Product } from './entity/Product';
+import { User } from './entity/User';
 
-export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "0000",
-    database: "deloreanzeta",
-    synchronize: true,
-    logging: false,
-    entities: [User, Category,Image, Product],
-    migrations: [],
-    subscribers: [],
-})
+// imports for typeorm
+import { DataSourceOptions } from 'typeorm';
 
-AppDataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
+// creating config for typeorm for mysql
+export const config: DataSourceOptions = {
+	type: 'mysql',
+	host: 'localhost',
+	port: 3306,
+	username: 'root',
+	password: '0000',
+	database: 'deloreanzeta',
+	entities: [Category, Product, User],
+	synchronize: true,
+	logging: false,
+};
