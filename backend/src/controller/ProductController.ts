dd94aa2d858beabccb79c1f93d86a02a6706f3e6
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2023-01-25 15:10:55 -0300

    paginado de productos

--- backend/src/controller/ProductController.ts
+++ backend/src/controller/ProductController.ts
@@ -1,97 +1,81 @@
-import { AppDataSource } from '../data-source';
-import { Request, Response } from "express";
-import { validate } from 'class-validator';
-import { Product } from "../entity/Product";
-import { CategoryController } from '../controller/CategoryController';
+import { Request, Response } from 'express';
+import { Product } from '../entity/Product';
 import { Category } from '../entity/Category';
-import { getManager } from 'typeorm';
+import { getProductRepository } from '../repositories/ProductRepository';
 
 export class ProductController {
+	static getAll = async (req: Request, res: Response) => {
+		const productRepository = getProductRepository();
+		const { page, limit } = req.query;
+		// parse page and limit to number
+		const pageInt = parseInt(page as string);
+		const limitInt = parseInt(limit as string);
+		// get products by page and limit
+		try {
+			const products = await productRepository.findAllByPage(
+				pageInt,
+				limitInt
+			);
+			res.send(products);
+		} catch (error) {
+			if (error.name === 'QueryFailedError') {
+				res.status(500).send({ message: 'Internal server error' });
+			} else {
+				res.status(400).send({ message: 'Bad request' });
+			}
+		}
+	};
 
-    static getAll = async (req: Request, res: Response) => {
-        const productRepository = AppDataSource.getRepository(Product);
-        const categoryRepository = AppDataSource.getRepository(Category);
-        let products;
-        let product;
-    
-        try {
-            products = await productRepository.query("SELECT * FROM product");//ProductRepository.find({ select: ['id', 'name', 'price', 'image', 'description', 'information', 'category'] });
-            console.log(typeof(products));
-        } catch (e) {
-            res.status(404).json({message: "Somenthing goes wrong ", e});
-        }
+	static getById = async (req: Request, res: Response) => {
+		const productRepository = AppDataSource.getRepository(Product);
 
-        if(products.length > 0){
-            let productsEdit = [];
-            for(let i = 0; i < products.length; i++){
-                product.id = products[i].id;
-                product.name = products[i].name;
-                product.price = products[i].price;
-                product.image = products[i].image;
-                product.description = products[i].description;
-                product.information = products[i].information;
-                product.category = await categoryRepository.findOneOrFail({where: {"id": products[i].category}});
-                productsEdit.push(product);
-                console.log(product);
-            }
-            /*crear array vacio
-            crear un product vacio
-            recorrer products y asignar todos sus campos al vacio , ademas del nombre de la categoria en lugar del id
-            guardar el product editado en el array vacio
-            retornar el array
-            */
+		try {
+			const product = await await productRepository.query(
+				'SELECT * FROM product WHERE id = ?',
+				[req.params.id]
+			); //productRepository.find({ select: ['id', 'name', 'price', 'image', 'description', 'information'], where: id });
+			res.send(product);
+		} catch (e) {
+			res.status(400).json({ message: 'Not result' });
+		}
+	};
 
-            res.send(productsEdit);
-        }else{
-            res.status(404).json({message: "Not results"});
-        }
-    };
+	static newProduct = async (req: Request, res: Response) => {
+		const { name, price, images, description, information, category } =
+			req.body;
+		const categoryRepository = AppDataSource.getRepository(Category);
+		//const productRepository = AppDataSource.getRepository(Product);
+		const productRepository = getProductRepository();
+		let categoryModel = new Category();
+		let product = new Product();
 
-    static getById = async (req: Request, res: Response) => {
+		try {
+			const categoryExist = await categoryRepository.find({
+				select: ['id', 'name'],
+				where: { id: category },
+			});
 
-        const productRepository = AppDataSource.getRepository(Product);
-        
-        try {
-            const product = await  await productRepository.query("SELECT * FROM product WHERE id = ?", [req.params.id]);//productRepository.find({ select: ['id', 'name', 'price', 'image', 'description', 'information'], where: id });
-            res.send(product);
-        } catch (e) {
-            res.status(400).json({message: "Not result"});
-        }
-    };
+			//console.log('contenido de categoriaExist ',categoryExist);
+			if (categoryExist.length > 0) {
+				product.name = name;
+				product.price = price;
+				product.description = description;
+				product.information = information;
+				categoryModel.id = categoryExist[0].id;
+				categoryModel.name = categoryExist[0].name;
 
-    static newProduct = async (req: Request, res: Response) => {
-        
-        const { name, price, image, description, information, category } = req.body;
-        const categoryRepository = AppDataSource.getRepository(Category);
-        const productRepository = AppDataSource.getRepository(Product);
-        let categoryModel = new Category();
-        let product = new Product();
+				product.category = categoryModel;
+				product.images = images;
 
-        try {
-            const categoryExist = await categoryRepository.find({select: ['id','name'], where: {"id": category}});
-            //console.log('contenido de categoriaExist ',categoryExist);
-            if( categoryExist.length > 0 ){
-
-                product.name = name;
-                product.price = price;
-                product.image = image;
-                product.description = description;
-                product.information = information;
-                categoryModel.id = categoryExist[0].id;
-                categoryModel.name = categoryExist[0].name;
-                
-                product.category = categoryModel;
-                
-                await productRepository.save(product);
-                
-            }else{
-                return res.status(404).json({message: "The category does not exist"});
-            }
-        } catch (e) {
-            return res.status(409).json(e);
-        }
-        res.send("Product created");
-    };
-
-
+				await productRepository.save(product);
+			} else {
+				return res
+					.status(404)
+					.json({ message: 'The category does not exist' });
+			}
+		} catch (e) {
+			return res.status(409).json(e);
+		}
+		res.send('Product created');
+	};
 }
