c65ff1ef24007f369f869e0cb852e38d8aef36d2
Axel147 <lopez.b.axel@gmail.com>
2023-01-29 03:43:49 -0300

    se agregaron validaciones para tours

--- backend/src/routes/tour.ts
+++ backend/src/routes/tour.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { TourController } from "../controller/TourController";
-//import { validateTour } from "../middlewares/tour.validator";
+import { validateTour } from "../middlewares/tour.validator";
 import { checkRole } from "../middlewares/role";
 
 const router = Router();
@@ -9,9 +9,9 @@ router.get("/", [/*checkJwt, checkRole(['admin'])*/], TourController.getAll);
 
 router.get("/:id", [/*checkJwt, checkRole(['admin'])*/], TourController.getById);
 
-router.post("/"/*, validateTour*/, [/*checkJwt, checkRole(['admin'])*/], TourController.newTour);
+router.post("/", validateTour, [/*checkJwt, checkRole(['admin'])*/], TourController.newTour);
 
-router.patch("/:id"/*, validateTour*/, [/*checkJwt, checkRole(['admin'])*/], TourController.editTour);
+router.patch("/:id", validateTour, [/*checkJwt, checkRole(['admin'])*/], TourController.editTour);
 
 router.delete("/:id", [/*checkJwt, checkRole(['admin'])*/], TourController.deleteTour);
 
