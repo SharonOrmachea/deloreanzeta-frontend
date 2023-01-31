92ab540984ba8cd0076b44713e2d167c836ea31b
Sharon <sharon.ormachea@gmail.com>
2023-01-30 22:32:59 -0300

    Arreglos Tours
    Organizamos en pair-programming con Belén el codigo de Tours y le hicimos algunos arreglos en el SASS

--- frontend/src/app/public/about-us/about-us.module.ts
+++ frontend/src/app/public/about-us/about-us.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AboutUsComponent } from './pages/about-us.component';
-import { UsComponent } from './components/us/us.component';
-import { DrummerComponent } from './components/drummer/drummer.component';
-import { GuitaristComponent } from './components/guitarist/guitarist.component';
 import { MembersComponent } from './components/members/members.component';
 import { OurTeamComponent } from './components/our-team/our-team.component';
 
@@ -12,9 +10,6 @@ import { OurTeamComponent } from './components/our-team/our-team.component';
 @NgModule({
   declarations: [
     AboutUsComponent,
-    UsComponent,
-    DrummerComponent,
-    GuitaristComponent,
     MembersComponent,
     OurTeamComponent
   ],
