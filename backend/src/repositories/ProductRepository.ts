1db558e800735065b1e410362551a8ac638359aa
Axel147 <lopez.b.axel@gmail.com>
2023-02-23 18:44:43 -0300

    se trae un producto por id

--- backend/src/repositories/ProductRepository.ts
+++ backend/src/repositories/ProductRepository.ts
@@ -19,14 +19,35 @@ const ProductRepository = conn.getRepository(Product).extend({
 		return products.map(product => ({
 			...product,
 			imageUrl: product.imageUrl.map(image => (image.data))
-		  }));
+		}));
 	},
 	async findByName(name: string): Promise<Product> {
 		return await this.findOneBy({ name });
 	},
-	async findById(id: number): Promise<Product> {
+	/*async findById(id: number): Promise<Product> {
 		return await this.findOneBy({ id });
+	},*/
+	async findById(id: number): Promise<Product> {
+		const product = await this.createQueryBuilder('product')
+		.leftJoinAndSelect('product.imageUrl', 'image')
+		.leftJoinAndSelect('product.category', 'category')
+		.select(['product', 'image.data', 'category.name'])
+		.where("product.id = :id", {id})
+		.getMany(); 
+		return product.map(product => ({
+			...product,
+			imageUrl: product.imageUrl.map(image => (image.data))
+		}));
 	},
+	/*async findByCategory(category: string): Promise<Product[]> {
+		// using query builder to get all products by category with name
+		const products = await this.createQueryBuilder('product')
+			.leftJoinAndSelect('product.category', 'category')
+			.where('category.name = :category', { category })
+			.getMany();
+		return products;
+	},
+	*/
 	async findByCategory(category: string): Promise<Product[]> {
 		// using query builder to get all products by category with name
 		const products = await this.createQueryBuilder('product')
@@ -35,6 +56,8 @@ const ProductRepository = conn.getRepository(Product).extend({
 			.getMany();
 		return products;
 	},
+
+
 	async saveProduct(product: Product): Promise<Product> {
 		return await this.save(product);
 	},
